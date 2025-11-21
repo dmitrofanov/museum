57b9e2010160e3d3781a899fea787bd948857f79
Dmitry Mitrofanov <mail@dmitrof.com>
2025-11-21 17:38:16 +0300

    Make dockerized version to be able to run locally

--- backend/museum_backend/museum_backend/settings.py
+++ backend/museum_backend/museum_backend/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY","django-insecure-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","localhost,127.0.0.1").split(",")
+
+IS_PROD = bool(os.environ.get("IS_PROD", default=0))
 
 
 # Application definition
@@ -150,10 +152,12 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 20
 }
 
-# CORS Settings
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "http://127.0.0.1:3000",
-# ]
+if IS_PROD:
+    CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',')
+    CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
 
-CORS_ALLOW_ALL_ORIGINS = True  # DEV ONLY!
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+else:
+    CORS_ALLOW_ALL_ORIGINS = True
